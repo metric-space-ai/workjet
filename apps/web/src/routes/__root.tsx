70385833ab7e125d1d9ae6f873b51d7867a39fe3
Michael Welsch <metricspace.ai@gmail.com>
2026-08-20 02:20:02 +0200

    feat(web): first-launch user-data migration offer dialog

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -15,6 +15,7 @@ import { resolveServerBackedAppDisplayName } from "../branding.logic";
 import { AppSidebarLayout } from "../components/AppSidebarLayout";
 import { CommandPalette } from "../components/CommandPalette";
 import { ConfirmDialogHost } from "../components/ConfirmDialogHost";
+import { UserDataMigrationDialog } from "../components/desktop/UserDataMigrationDialog";
 import { ConnectOnboardingDialog } from "../components/cloud/ConnectOnboardingDialog";
 import { RelayClientInstallDialog } from "../components/cloud/RelayClientInstallDialog";
 import { SshPasswordPromptDialog } from "../components/desktop/SshPasswordPromptDialog";
@@ -136,6 +137,7 @@ function RootRouteView() {
         <RelayClientInstallDialog />
         <ConnectOnboardingDialog />
         <SshPasswordPromptDialog />
+        <UserDataMigrationDialog />
         <ConfirmDialogHost />
         <SlowRpcRequestToastCoordinator />
         <HostedStaticEnvironmentBootstrap />
